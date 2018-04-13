035201cc58c38373319f873caa8b6bcb5262f39d
Frank-Tao-Oracle <zhiweitao.suzhou@gmail.com>
2018-04-13 19:01:31 +0800

    change the style of dashboard

--- JETUI/OracleInteractUI/public_html/js/viewModels/dashboard.js
+++ JETUI/OracleInteractUI/public_html/js/viewModels/dashboard.js
@@ -103,6 +103,10 @@ define(['ojs/ojcore', 'knockout', 'jquery'],
           });
         return 0;
     }, this);
+    self.createChat = function(){
+        window.location.href = "http://localhost:8081/index.html?root=chat";
+        
+    };
     }
 
     return new DashboardViewModel();
