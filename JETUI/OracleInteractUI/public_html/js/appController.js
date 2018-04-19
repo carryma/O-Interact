c3f557c22bad802e6b4e03222cb6001b7284aa10
Kang Ma <kang.ma@oracle.com>
2018-04-20 02:56:00 +0800

    qroage

--- JETUI/OracleInteractUI/public_html/js/appController.js
+++ JETUI/OracleInteractUI/public_html/js/appController.js
@@ -6,68 +6,83 @@
  * Your application specific code will go here
  */
 define(['ojs/ojcore', 'knockout', 'ojs/ojrouter', 'ojs/ojknockout', 'ojs/ojarraytabledatasource'],
-        function (oj, ko) {
-            function ControllerViewModel() {
-                var self = this;
+    function (oj, ko) {
+        function ControllerViewModel() {
+            var self = this;
 
-                // Media queries for repsonsive layouts
-                var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
-                self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+            // Media queries for repsonsive layouts
+            var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
+            self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
 
-                // Router setup
-                self.router = oj.Router.rootInstance;
-                self.router.configure({
-                    'navigation': {label: 'Navigation'},
-                    'chat': {label: 'Chat'},
-                    'customers': {label: 'Customers'},
-                    'lottery': {label: 'Lottery'},
-                    'vote': {label: 'Vote'},
-                    'login': {label: 'Login', isDefault: true},
-                    'largescreen': {label: 'Largescreen'},
-                    'qrcode': {label: 'QrCode'}
-                });
-                oj.Router.defaults['urlAdapter'] = new oj.Router.urlParamAdapter();
+            // Router setup
+            self.router = oj.Router.rootInstance;
+            self.router.configure({
+                'navigation': { label: 'Navigation' },
+                'chat': { label: 'Chat' },
+                'customers': { label: 'Customers' },
+                'lottery': { label: 'Lottery' },
+                'vote': { label: 'Vote' },
+                'login': { label: 'Login', isDefault: true },
+                'largescreen': { label: 'Largescreen' },
+                'qrcode': { label: 'QrCode' }
+            });
+            oj.Router.defaults['urlAdapter'] = new oj.Router.urlParamAdapter();
 
-                // Navigation setup
-                var navData = [
-                    {name: 'Login', id: 'login',
-                        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'},
-                    {name: 'Navigation', id: 'navigation',
-                        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'},
-                    {name: 'Incidents', id: 'incidents',
-                        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24'},
-                    {name: 'Customers', id: 'customers',
-                        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24'},
-                    {name: 'Lottery', id: 'lottery',
-                        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'},
-                    {name: 'Largescreen', id: 'largescreen',
-                        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'},
-                    {name: 'QrCode', id: 'qrcode',
-                        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'}
-                ];
-                self.navDataSource = new oj.ArrayTableDataSource(navData, {idAttribute: 'id'});
-
-                // Header
-                // Application Name used in Branding Area
-                self.appName = ko.observable("Oracle-Interact");
-                // User Info used in Global Navigation area
-                self.userLogin = ko.observable("john.hancock@oracle.com");
-
-                // Footer
-                function footerLink(name, id, linkTarget) {
-                    this.name = name;
-                    this.linkId = id;
-                    this.linkTarget = linkTarget;
+            // Navigation setup
+            var navData = [
+                {
+                    name: 'Login', id: 'login',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'
+                },
+                {
+                    name: 'Navigation', id: 'navigation',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'
+                },
+                {
+                    name: 'Incidents', id: 'incidents',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24'
+                },
+                {
+                    name: 'Customers', id: 'customers',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24'
+                },
+                {
+                    name: 'Lottery', id: 'lottery',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'
+                },
+                {
+                    name: 'Largescreen', id: 'largescreen',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'
+                },
+                {
+                    name: 'QrCode', id: 'qrcode',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'
                 }
-                self.footerLinks = ko.observableArray([
-                    new footerLink('About Oracle', 'aboutOracle', 'http://www.oracle.com/us/corporate/index.html#menu-about'),
-                    new footerLink('Contact Us', 'contactUs', 'http://www.oracle.com/us/corporate/contact/index.html'),
-                    new footerLink('Legal Notices', 'legalNotices', 'http://www.oracle.com/us/legal/index.html'),
-                    new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
-                    new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
-                ]);
-            }
+            ];
+            self.navDataSource = new oj.ArrayTableDataSource(navData, { idAttribute: 'id' });
 
-            return new ControllerViewModel();
+            // Header
+            // Application Name used in Branding Area
+            self.appName = ko.observable("Oracle-Interact");
+            self.userLogin = ko.observable("login");
+            //  }else{
+            //    self.userLogin = ko.observable(username);
+            // }
+            // Footer
+            function footerLink(name, id, linkTarget) {
+                this.name = name;
+                this.linkId = id;
+                this.linkTarget = linkTarget;
+            }
+            self.footerLinks = ko.observableArray([
+                new footerLink('About Oracle', 'aboutOracle', 'http://www.oracle.com/us/corporate/index.html#menu-about'),
+                new footerLink('Contact Us', 'contactUs', 'http://www.oracle.com/us/corporate/contact/index.html'),
+                new footerLink('Legal Notices', 'legalNotices', 'http://www.oracle.com/us/legal/index.html'),
+                new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
+                new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
+            ]);
         }
+
+        return new ControllerViewModel();
+    }
 );
