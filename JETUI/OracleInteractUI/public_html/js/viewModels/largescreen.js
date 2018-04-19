ffbf8f5833bcdcbb2b47ebebd52d9fcce09f9afa
Kang Ma <kang.ma@oracle.com>
2018-04-19 18:35:50 +0800

    修改样式表

--- JETUI/OracleInteractUI/public_html/js/viewModels/largescreen.js
+++ JETUI/OracleInteractUI/public_html/js/viewModels/largescreen.js
@@ -8,34 +8,38 @@
  * largeScreen module
  */
 define(['ojs/ojcore', 'knockout', 'jquery', 'socket.io'],
- function(oj, ko, $, io) {
-    /**
-     * The view model for the main content view template
-     */
-    function largeScreenContentViewModel() {
+    function (oj, ko, $, io) {
+        /**
+         * The view model for the main content view template
+         */
         var self = this;
-        $(function () {
-        function randomColor(){//随机颜色  
-            var color=Math.ceil(Math.random()*16777215).toString(16);  
-            while(color.length < 6){  
-                color = "0" + color;  
-            }  
-            return color;  
-        }  
-        
-        var socket = io();
-	socket.on('receiveMessage', function (data) {
-                var node = $('<div class=move>'+data.message+'</div>')
-                node.css('top', Math.random()*400+"px");
-                node.css('color', "#"+randomColor());
-                node.css('font-size',"24px");
-                node.bind('animationend', function(event) {
-                    $(event.target).remove();
+        self.redirectToChat = function () {
+            window.location.href = "http://localhost:8081/index.html?root=chat";
+        };
+        function largeScreenContentViewModel() {
+            var self = this;
+            $(function () {
+                function randomColor() {//随机颜色  
+                    var color = Math.ceil(Math.random() * 16777215).toString(16);
+                    while (color.length < 6) {
+                        color = "0" + color;
+                    }
+                    return color;
+                }
+
+                var socket = io();
+                socket.on('receiveMessage', function (data) {
+                    var node = $('<div class=move>' + data.message + '</div>')
+                    node.css('top', Math.random() * 400 + "px");
+                    node.css('color', "#" + randomColor());
+                    node.css('font-size', "24px");
+                    node.bind('animationend', function (event) {
+                        $(event.target).remove();
+                    });
+                    $("#barrage").append(node);//插入子节点
                 });
-                $("#barrage").append(node);//插入子节点
             });
-        });
-    }
-    
-    return largeScreenContentViewModel;
-});
+        }
+
+        return largeScreenContentViewModel;
+    });
